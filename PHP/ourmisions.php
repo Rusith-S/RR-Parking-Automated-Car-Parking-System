75edc55042a7f523e16840e313aae8b364cbda1f
Rusith Sashika <158534064+Rusith2001@users.noreply.github.com>
2024-02-05 20:43:32 +0530

    Create ourmisions.php

---
+++ PHP/ourmisions.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+
+
+<link rel="stylesheet" href="../css/ourmision.css">
+<title>RR Parking - Login</title>
+
+
+<body>
+<!-- header -->
+ <header>
+    <img src="../images/header-footer/Logo.jpg" class="logo">
+        <nav>
+            <ul class="links ul-nav">
+                <li class="links"><a href="./index.php">Home</a></li>
+                <li class="links"><a href="./FAQs.php">FAQ's</a></li>
+                <li class="links"><a href="./About us.php">About us </a></li>
+                <li class="links"><a href="./privacypolicy.php">Privacy policy </a></li>
+                <li class="links"><a href="./reviews.php">Customer reviews </a></li>
+                <li class="links"><a href="./contactt.php">Contact us</a></li>
+                <li class="links"><a href="./offer.php">Offers</a></li>
+                <li class="links"><a href="./location.php">Locations</a></li>
+            </ul>
+        </nav>
+    <a href="./login.php" class="account">
+        <button class="btn-primary"> Log in </button>
+    </a> 
+    <div></div>
+</header>
+<!-- end: header -->
+
+
+
+
+<div class="about-section">
+  <div class="inner-cotainer">
+      <h1>Our Mission</h1>
+      <p class="text">
+        RR parking systems ultimate goal is to provide the anytime anywhere 
+        parking accessibility to the customers while ensuring the safety of their vehicles.
+         Furthermore, our parking system is more efficient, convenient, affordable 
+         to anyone when compared with conventional parking. So we ensure to fulfill 
+         the needs of the customers while proving them with the best service.
+      </p>
+      <div class="skills">
+          <span>
+           Secured
+          </span>
+          <span>
+           Affordable
+          </span>
+          <span>
+           Convenient
+          </span>
+          <span>
+          Accessible 
+          </span>
+      </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="copy">
+    <p> &copy; COPYRIGHT @RRparking.com | 2020 - 2022 </p>
+</div>
+<!-- end: footer -->
+</body>
+
+</html>
